15e86af8f897fba6ebe24ca797e1fb13fbb7acbb
Bogdan Tabusca <btabusca@endava.com>
2019-04-03 18:34:05 +0300

    Connected to DB, Read and Create are now functional.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace ConsoleApp1
+namespace DatabaseApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DatabaseManager dbManager = new DatabaseManager("D3hbc2J4iw4OrRPzwV9BFxs669KSIdp6jvbj7knf",
+                "https://cssproj-fbcd0.firebaseio.com/");
+            var columns=new List<string>();
+            columns.Add("Insects");
+            columns.Add("Birds");
+            columns.Add("Fishes");
+            columns.Add("Mammals");
+            columns.Add("Crustaceans");
+
+            var crustaceans = new List<string>();
+            crustaceans.AddRange(new []{"Shrimp","Crab","Squid"});
+            //dbManager.CreateTable("Animals", columns).GetAwaiter().GetResult();
+            //dbManager.CreateOrUpdateColumn("Animals", "Crustaceans", crustaceans).GetAwaiter().GetResult();
+            dbManager.AppendToColumn("Animals", "Crustaceans",crustaceans).GetAwaiter().GetResult();
+            var table= dbManager.ReadTable("Animals").GetAwaiter().GetResult();
+            Console.WriteLine("Done");
+            //var table = dbManager.ReadTable("Animals").GetAwaiter().GetResult();
+            Console.ReadLine();
         }
     }
 }
