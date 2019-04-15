8d2b395452c18efc6ee633cece64f53b380edd33
Bogdan Tabusca <btabusca@endava.com>
2019-04-15 15:43:41 +0300

    Added Database Functions for creating a db , Create Read and delete Table, a json file per Table (jObject)

---
+++ SimpleDatabase/DatabaseKeeper/DataKeeper.cs
@@ -0,0 +1,117 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DatabaseKeeper
+{
+    public class DataKeeper
+    {
+        private Dictionary<string, string> DatabasesList;
+        private Dictionary<string, List<string>> DatabaseTables;
+        private string databaseName;
+
+        public DataKeeper()
+        {
+            DatabasesList=new Dictionary<string, string>();
+            DatabaseTables = new Dictionary<string, List<string>>();
+        }
+
+        public void CreateDatabase(string path)
+        {
+            FileInfo file = new FileInfo(path+$"\\{databaseName}\\{databaseName}.txt");
+            file.Directory.Create();
+            DatabasesList.Add(databaseName, path+$"\\{databaseName}\\");
+        }
+
+        // Dangerous Method !UNTESTED!
+        /*
+        public void DeleteDatabase(string path)
+        {
+            FileInfo file = new FileInfo(path + $"\\{databaseName}\\{databaseName}.txt");
+            file.Directory.Delete();
+            DatabasesList.Remove(databaseName);
+            databaseName = "";
+        }*/
+
+        public void LoadDatabase(string databaseName, string path)
+        {
+            DatabasesList.Add(databaseName,path+$"\\{databaseName}\\");
+
+            DirectoryInfo directory = new DirectoryInfo(path + $"\\{databaseName}");
+            FileInfo[] files = directory.GetFiles();
+
+            DatabaseTables.Add(databaseName,new List<string>());
+            foreach (var table in files)
+            {
+                DatabaseTables[databaseName].Add(table.Name);
+            }
+            
+        }
+
+        public void SelectDatabase(string databaseName)
+        {
+            this.databaseName = databaseName;
+        }
+
+        public void CreateTable(string tableName, List<string> columns)
+        {
+            if(!DatabasesList.ContainsKey(databaseName))
+                throw new Exception("Database Not Loaded!");
+            
+            JObject table = new JObject(
+                from column in columns select new JProperty(column,new JArray())
+                );
+
+            var path = DatabasesList[databaseName]+tableName+".json";
+
+            var file = File.Open(path, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            var writer = new StreamWriter(file);
+            writer.Write(table.ToString());
+            writer.Flush();
+            file.Close();
+
+            DatabaseTables[databaseName].Add(tableName);
+        }
+
+        public void DeleteTable(string tableName)
+        {
+            if (!DatabasesList.ContainsKey(databaseName))
+                throw new Exception("Database Not Loaded!");
+            if (!DatabaseTables[databaseName].Contains(tableName))
+                throw new Exception("Table dose not exist!");
+
+            var path = DatabasesList[databaseName] + tableName + ".json";
+
+            File.Delete(path);
+        }
+
+        public JObject ReadTable(string tableName)
+        {
+            try
+            {
+                JObject table;
+
+                var path = DatabasesList[databaseName] + tableName + ".json";
+
+                using (StreamReader file = File.OpenText(path))
+                using (JsonTextReader jreader = new JsonTextReader(file))
+                {
+                    table = (JObject) JToken.ReadFrom(jreader);
+                }
+
+                return table;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+
+    }
+}
---
+++ SimpleDatabase/DatabaseKeeper/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseKeeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataKeeper dk=new DataKeeper();
+            var columns = new List<string>();
+            columns.AddRange(new []{"Col1","Col2","Col3"});
+
+            //dk.CreateDatabase("AJsonDB", @"C:\scrap");
+            dk.LoadDatabase("AJsonDB", @"C:\scrap");
+            dk.SelectDatabase("AJsonDB");
+
+            dk.CreateTable("MyFirstTable",columns);
+            //dk.DeleteTable("MyFirstTable");
+            var table = dk.ReadTable("MyFirstTable");
+        }
+    }
+}
