ac67ef0044536ac8a6d6426f3b58621266314f6f
Lucian Toma <tomalucian11@gmail.com>
2019-04-22 14:26:21 +0300

    Finish Import button functionality

--- SimpleDatabase/SimpleDatabase/MainWindow.xaml.cs
+++ SimpleDatabase/SimpleDatabase/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using DatabaseKeeper;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace SimpleDatabase
 {
@@ -43,23 +45,46 @@ namespace SimpleDatabase
             var table = dk.ReadTable(TABLE_NAME);
             //dk.AddEntries(TABLE_NAME, "Col3", values);
             //dk.AddColumns(TABLE_NAME, ncolumns);
+            List<string> columnNames = keeper.GetColumnNames(TABLE_NAME);
             var column1Entries = dk.ReadColumn(TABLE_NAME, "Col1");
             var column2Entries = dk.ReadColumn(TABLE_NAME, "Col2");
-            
 
-            dataGrid.ItemsSource = (List<string>)table;
-            Console.WriteLine("ReadTable:");
-            foreach (string column in (List<string>)table) {
-                Console.WriteLine($"{column}:");
-                //foreach (string item in (List<string>)column1Entries)
-                //{
-                //    Console.WriteLine(item);
-                //}
+            Dictionary<string, List<string>> columnData = new Dictionary<string, List<string>>();
+
+            // DataGrid - Init DataTable
+            DataTable dataTable = new DataTable();
+
+            // DataGrid - Init Columns
+            Console.WriteLine("Columns:");
+            foreach (string columnName in columnNames)
+            {
+                DataColumn dataColumn = new DataColumn(columnName, typeof(string));
+                dataTable.Columns.Add(dataColumn);
+
+                List<string> columnValues = dk.ReadColumn(TABLE_NAME, columnName);
+                columnData[columnName] = columnValues;
             }
 
 
-            //Importer.Importer importer= new Importer.Importer();
-            //var parsedcsv = importer.ReadCsv(@"C:\scrap\AJsonDB\exampleCSV.csv");
+            /// DataGrid - Init Rows
+            // Get number of row
+            int rowCount = MaxCount(columnData);
+            // Populate rows
+            for (int rowIndex = 0; rowIndex < rowCount; ++rowIndex)
+            {
+                DataRow row = dataTable.NewRow();
+                for (int columnIndex = 0; columnIndex < columnData.Keys.Count; ++columnIndex)
+                {
+                    string currentColumn = columnNames[columnIndex];
+                    if (rowIndex < columnData[currentColumn].Count)
+                    {
+                        row[columnIndex] = columnData[currentColumn][rowIndex];
+                    }
+                }
+                dataTable.Rows.Add(row);
+            }
+            
+            dataGrid.ItemsSource = dataTable.DefaultView;
         }
 
         private void Button_Export_Click(object sender, RoutedEventArgs e)
@@ -75,6 +100,20 @@ namespace SimpleDatabase
         private void Button_DeleteItem_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        // Returns the max item count out of each item list in the dictionary
+        private int MaxCount<T>(Dictionary<T, List<T>> dict)
+        {
+            int max = 0;
+            foreach (List<T> list in dict.Values)
+            {
+                if (list.Count > max)
+                {
+                    max = list.Count;
+                }
+            }
+            return max;
         }
     }
 }
